2ba50bf15ef83e9a0dd44973078824f2e2393adc
ElkinC1 <elkincanto999@gmail.com>
2026-06-23 00:03:30 -0500

    Refactorizar y mejorar la estructura del proyecto

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,18 +1,18 @@
 using Application.Departamentos.Queries;
+using Domain;
 using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddInfrastructure();
 builder.Services.AddScoped<IDepartamentoGetAll, DepartamentoGetAll>();
 builder.Services.AddScoped<IDepartamentoGetById, DepartamentoGetById>();
-
-builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
@@ -20,16 +20,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-        using var scope = app.Services.CreateScope();
+    using var scope = app.Services.CreateScope();
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
     dbContext.Database.EnsureDeleted();
     dbContext.Database.EnsureCreated();
     if (!dbContext.Departamentos.Any())
     {
         dbContext.Departamentos.AddRange(
-            new Domain.Departamento { Nombre = "Departamento 1" },
-            new Domain.Departamento { Nombre = "Departamento 2" },
-            new Domain.Departamento { Nombre = "Departamento 3" }
+            new Departamento { Nombre = new Nombre("Departamento 1") },
+            new Departamento { Nombre = new Nombre("Departamento 2") },
+            new Departamento { Nombre = new Nombre("Departamento 3") }
         );
         dbContext.SaveChanges();
     }
@@ -46,7 +46,6 @@ app.MapGet(
         return Results.Ok(departamento);
     }
 );
-
 app.MapGet(
         "/departamentos",
         async (IDepartamentoGetAll departamentoGetAll) =>
@@ -57,8 +56,6 @@ app.MapGet(
     )
     .WithName("GetDepartamentos");
 
-
 app.UseHttpsRedirection();
-
 
 app.Run();
