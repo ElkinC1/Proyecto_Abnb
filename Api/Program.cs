034a2eb04761b3d792b45cfff0c82b8b12430f78
ElkinC1 <elkincanto999@gmail.com>
2026-06-22 23:22:32 -0500

    Agregar funcionalidad para gestionar departamentos

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,15 @@
+using Application.Departamentos.Queries;
+using Domain;
+using Infrastructure.Data.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IDepartamentoGetAll, DepartamentoGetAll>();
+builder.Services.AddScoped<IDepartamentoRepository, DepartamentoRepository>();
 
 var app = builder.Build();
 
@@ -11,43 +18,18 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+app.MapGet(
+        "/departamentos",
+        async (IDepartamentoGetAll departamentoGetAll) =>
+        {
+            var departamentos = await departamentoGetAll.Execute();
+            return Results.Ok(departamentos);
+        }
+    )
+    .WithName("GetDepartamentos");
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing",
-    "Bracing",
-    "Chilly",
-    "Cool",
-    "Mild",
-    "Warm",
-    "Balmy",
-    "Hot",
-    "Sweltering",
-    "Scorching",
-};
-
-app.MapGet(
-        "/weatherforecast",
-        () =>
-        {
-            var forecast = Enumerable
-                .Range(1, 5)
-                .Select(index => new WeatherForecast(
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-                .ToArray();
-            return forecast;
-        }
-    )
-    .WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- Domain/Departamento.cs
+++ Domain/Departamento.cs
@@ -1,8 +1,16 @@
 ﻿namespace Domain;
 
-public class Departamento 
+public class Departamento
 {
-    public int Id {get; set;}
-    public string Nombre {get; set;} = string.Empty;
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
 }
 
+public interface IDepartamentoRepository
+{
+    Task<List<Departamento>> GetAllAsync();
+    Task<Departamento?> GetByIdAsync(int id);
+    Task AddAsync(Departamento departamento);
+    Task UpdateAsync(Departamento departamento);
+    Task DeleteAsync(int id);
+}
